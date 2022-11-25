9de18c3fd36d98aacb4cd533493060ad125f77a4
Milena Kmiecik <mkmiecik1@sii.pl>
2022-11-25 10:53:41 +0100

    cards component separated

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Ingredient} from "../models/ingredient.model";
+import {PizzaService} from "../services/pizza.service";
+import {IngredientsService} from "../services/ingredients.service";
+import {ReadyPizzaView} from "../models/readypizza.model";
 
 @Component({
   selector: 'app-cards',
@@ -6,10 +10,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
+  ingredients: Ingredient[] = this.ingredientService.getAllIngredients();
+  title = 'my-pizza-story';
+  readyPizza: ReadyPizzaView = {
+    title: '',
+    ingredients: [],
+    votes: 0,
+  }
 
-  constructor() { }
+  constructor(private ingredientService: IngredientsService,
+              private pizzaService: PizzaService) {
+  }
 
   ngOnInit(): void {
   }
 
+  addIngredient(ingredient: Ingredient) {
+    this.readyPizza.ingredients.push(ingredient);
+    console.log(this.readyPizza.ingredients)
+  }
+
+  deleteIngredient(ingredient: Ingredient) {
+    const indexToDelete: number = this.readyPizza.ingredients.indexOf(ingredient)
+    if (indexToDelete > -1) {
+      this.readyPizza.ingredients.splice(indexToDelete, 1)
+    }
+    console.log(this.readyPizza.ingredients)
+  }
+
+  addPizza() {
+    this.pizzaService.addPizza(this.readyPizza);
+    console.log(this.pizzaService.getAllPizzas());
+  }
+
+  getIngredientsCount(): number {
+    return this.readyPizza.ingredients.length;
+  }
+
+  checkNameAvailability(title: string): boolean {
+    return this.getAllPizzas()
+      .filter(pizza => pizza.title === title).length == 0;
+  }
+
+  getAllPizzas() {
+    return this.pizzaService.getAllPizzas();
+  }
 }
